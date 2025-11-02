ba7f7c963e36159fff20ad782f34dec3b99bc9a2
”ns-develops” <test.tt9061030@gmail.com>
2025-11-02 10:15:21 +0100

    adding router

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -25,10 +25,68 @@ export const register = async (req, res) =>{
 
     res.cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production'
-    })
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'development' ?
+        'node' : 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
+        return res.json({success: true});
 
     } catch (error) {
         res.json({success: false, message: error.message})
     }
 } 
+
+export const login = async (req, res) => {
+    const{email, password} = req.body;
+
+    if(!email || !password) {
+    return res.json({success:false, message: 'Email and password are required'})
+ }
+
+try {
+    const user = await userModel.findOne({email});
+
+    if(!user) {
+        return res.json({success: false, message: 'Invalid email'})
+    } 
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch) {
+        return res.json({success: false, message: 'Invalid password'})
+    }
+
+    const token = jwt.sign({id: user._id_}, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'development' ?
+        'node' : 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
+    return res.json({success: true});
+    
+
+}  catch (error) {
+    res.json({success: false, message: error.message})
+}
+} 
+
+export const logout = async (req, res)=>{
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'development' ?
+            'node' : 'strict',
+        })
+
+        return res.json({success: true, message: "Logged out"})
+}  catch (error) {
+    res.json({success: false, message: error.message});
+   } 
+}   
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
+import authRouter from './routes/authRoutes.js'
 
 import connectDB from "./config/mongodb.js";
 
@@ -16,6 +17,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: true }));
 
+//API Endpoints
 app.get('/', (req, res) => res.send("API Working"));
+app.use('/api/auth', authRouter)
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
